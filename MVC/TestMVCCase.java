2a8734e897857ed3ef0e03d6eb7c14b617e8d452
McBride-Devin <dapolevaulter@gmail.com>
2016-10-26 17:47:03 -0600

    Update TestMVCCase.java

--- MVC/TestMVCCase.java
+++ MVC/TestMVCCase.java
@@ -1,3 +1,26 @@
 public class TestMVCCase {
-	
+	public static void main(String[] args) {
+
+      //fetch student record based on his roll no from the database
+      Student model  = retriveJEFromDb();
+
+      //Create a view : to write student details on console
+      JournalView view = new JournalView();
+
+      JournalController controller = new JournalController(model, view);
+
+      controller.updateView();
+
+      //update model data
+      controller.setSubject("Test");
+
+      controller.updateView();
+   }
 }
+   private static Student retriveJEFromDb(){
+      Journal JournalEntry = new JournalEntry();
+      journal.setSubject("How to Use MVC");
+      journal.setEntry("This is a test case for retrieving a Journal Entry from a fake database");
+      return JournalEntry;
+   }
+}	
